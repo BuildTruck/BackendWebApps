55846a7dc6f1ecba06564b7e5582b8f0000416b8
matthewsrt29 <matthew.tr29@gmail.com>
2025-06-13 14:48:16 -0500

    feat: enviroments added again

--- BuildTruckBack/obj/Debug/net9.0/BuildTruckBack.AssemblyInfo.cs
+++ BuildTruckBack/obj/Debug/net9.0/BuildTruckBack.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BuildTruckBack")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5127acd7bdae061afa5afc2d3966a4c9e4849a49")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7cc1f21b4cffff05bfd7535f25b11b8222e4ea7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("BuildTruckBack")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BuildTruckBack")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
